cf45f13b59ac4aa2f2a6a1240d699fd86f52c8fa
jieun kim <nc1315@naver.com>
2018-05-18 11:40:06 +0900

    특정 달 최대 일수 함수 따로 빼서

--- src/calendar/MaxDays.java
+++ src/calendar/MaxDays.java
@@ -3,15 +3,20 @@ package calendar;
 import java.util.Scanner;
 
 public class MaxDays {
-
+	
+	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	public int maxDaysOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		MaxDays cal = new MaxDays();
 		System.out.print("달을 입력해주세요. ");
 		int month = scan.nextInt();
 		
-		int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
-		
-		System.out.printf("%d월은 %d일까지 있습니다.",month,maxDays[month -1]);
+		System.out.printf("%d월은 %d일까지 있습니다.",month, cal.maxDaysOfMonth(month));
 		scan.close();
 	}
 
